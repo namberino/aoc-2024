b785f080a5bf8c895bb49497aca2c02457ae1165
namberino <namnb2402@gmail.com>
2024-12-11 16:30:27 +0700

    Add day 5 solution

--- day-5/solve.cpp
+++ day-5/solve.cpp
@@ -15,6 +15,7 @@ int main(int argc, char* argv[])
     std::ifstream input_file(argv[1]);
     std::string text;
     std::map<std::string, std::vector<std::string> > ordering_rules;
+    int sum_mid = 0;
 
     while (std::getline(input_file, text))
     {
@@ -44,13 +45,57 @@ int main(int argc, char* argv[])
         }
     }
 
-    for (auto item : ordering_rules)
+    // for (auto item : ordering_rules)
+    // {
+    //     std::cout << item.first << " : ";
+    //     for (auto element : item.second)
+    //         std::cout << element << " ";
+    //     std::cout << "\n";
+    // }
+
+    // thank god it continues reading where it left off in the file
+    while (std::getline(input_file, text))
     {
-        std::cout << item.first << " : ";
-        for (auto element : item.second)
-            std::cout << element << " ";
-        std::cout << "\n";
+        std::vector<int> update_pages;
+
+        // again, a bit hacky but there's only 2 digits in the page number soooooo
+        for (int i = 1; i < text.size(); i += 3)
+        {
+            std::string num;
+            num.push_back(text[i - 1]);
+            num.push_back(text[i]);
+
+            update_pages.push_back(stoi(num));
+        }
+
+        int mid_index = update_pages.size() / 2;
+        bool valid_update_list = true;
+
+        // no need to check the last element
+        for (int i = 0; i < update_pages.size() - 1; i++)
+        {
+            int current_num = update_pages[i];
+            std::vector<std::string> curr_page_rules = ordering_rules[std::to_string(current_num)];
+
+            // iterate through the rest of the vector
+            for (int j = i + 1; j < update_pages.size(); j++)
+            {
+                if (std::find(curr_page_rules.begin(), curr_page_rules.end(), std::to_string(update_pages[j])) == curr_page_rules.end())
+                {
+                    valid_update_list = false;
+                    break;
+                }
+            }
+
+            if (!valid_update_list)
+                break;
+        }
+
+        if (valid_update_list)
+            sum_mid += update_pages[mid_index];
     }
+
+    std::cout << "Sum of middle page in update lists: " << sum_mid << std::endl;
 
     input_file.close();
     return 0;
